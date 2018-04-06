7a5a174ac2a5abb0fb2f526196bc0ceafb0f067f
Malek Erwin Karim Mohd Zin <erwinkarim@gmail.com>
2018-04-06 13:36:12 +0800

    Move UserLanding other components into their own files

---
+++ src/components/UserLanding/EnrolledCourses.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import {
+  Row, CardColumns, Card, CardTitle,
+  CardBody, CardText, Button, Modal, ModalBody, ModalFooter,
+} from 'reactstrap';
+import { HashLink as Link } from 'react-router-hash-link';
+import PropTypes from 'prop-types';
+import Notice from '../../components/Notice';
+import '../../containers/UserLanding.css';
+import '../../containers/CertCheck.css';
+import config from '../../config';
+import { invokeApig } from '../../libs/awsLibs';
+
+/**
+ * The Constructor
+ * @param {json} e the props
+ * @returns {null} The sum of the two numbers.
+ */
+export default class EnrolledCourses extends Component {
+  /**
+   * The Constructor
+   * @param {json} props the props
+   * @returns {null} The sum of the two numbers.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [], isLoading: false, modal: false, certContents: {},
+    };
+  }
+  componentDidMount = async () => {
+    console.log('attempt to get enrolled courses');
+    try {
+      this.setState({ isLoading: true });
+      const results = await this.loadEnrolledCourses();
+      this.setState({ courses: results, isLoading: false });
+    } catch (e) {
+      console.log('error getting enrolled courses');
+      console.log(e);
+    }
+  }
+  loadEnrolledCourses = () => invokeApig({
+    endpoint: config.apiGateway.ENROLMENT_URL,
+    path: '/enrolment',
+  })
+  toggleModal = (e) => {
+    this.setState({ modal: !this.state.modal });
+
+    if (!this.state.modal) {
+      // should update cert info
+      const courseIndex = e.target.dataset.course;
+      this.setState({
+        certContents: this.state.courses[courseIndex],
+      });
+    }
+  }
+  render = () => {
+    if (this.state.isLoading) {
+      return <Notice content="Checking enrolled courses ..." />;
+    }
+
+    const enrolledCourses = this.state.courses.length === 0 ? (
+      <div>You haven&amp;t enrolled in any courses yet.</div>
+    ) : (
+      <CardColumns>{ this.state.courses.map((c, i) => {
+        const courseComplete = c.progress.length >= c.publishedModuleCount;
+        return (
+          <Card key={c.courseId} className={`${courseComplete ? 'border border-success' : ''}`}>
+            <CardBody>
+              <CardTitle><Link href="/" to={`/courses/toc/${c.courseId}`}>{c.name}</Link></CardTitle>
+              <CardText><strong>Progress</strong></CardText>
+              <CardText>{c.progress.length} of {c.publishedModuleCount} modules attended</CardText>
+              {
+                courseComplete ?
+                  <CardText className="text-success">
+                    Course Completed <Button color="info" size="small" onClick={this.toggleModal} data-course={i}>View Cert</Button>
+                  </CardText> : null
+              }
+            </CardBody>
+          </Card>);
+      })}
+      </CardColumns>
+    );
+
+    const certDate = new Date(this.state.certContents.certIssued);
+    const courseName = this.state.certContents.name ?
+      this.state.certContents.name.toUpperCase() : null;
+    return (
+      <Row>
+        <div className="col-12">
+          <h3>Enrolled Courses</h3>
+          <hr />
+          { enrolledCourses }
+          <Modal isOpen={this.state.modal} onOpened={this.handleModalOpen} size="lg" >
+            <ModalBody className="cert text-center">
+              <h2>CERTIFICATE OF COMPLETION</h2>
+              <p className="cert-text">This certifies that</p>
+              <p>{ this.props.currentUser.name }</p>
+              <p className="cert-text">has completed the short course entitled</p>
+              <p><strong>{ courseName }</strong></p>
+              <p><img className="cert-logo" src="/logos/learn.part1.png" alt="learn@AP" /></p>
+              <div className="w-100 d-flex justify-content-between  pb-4">
+                <span className="cert-text col-4 pt-4">
+                  <Link href="/" to={`/verify_cert?certNo=${this.state.certContents.certId}`}>
+                    <small>{this.state.certContents.certId}</small>
+                  </Link>
+                </span>
+                <span className="col-4" />
+                <span className="cert-text col-4 pt-4">{`${certDate.toLocaleString('en-us', { day: 'numeric', month: 'long', year: 'numeric' })}`}</span>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggleModal}>OK</Button>
+            </ModalFooter>
+          </Modal>
+          <div className="col-12">
+            <p><Button color="primary" to="/courses" tag={Link}>Explore Courses</Button></p>
+          </div>
+        </div>
+      </Row>
+    );
+  }
+}
+
+EnrolledCourses.propTypes = {
+  currentUser: PropTypes.shape().isRequired,
+};
--- src/containers/UserLanding.js
+++ src/containers/UserLanding.js
@@ -1,178 +1,16 @@
 import React, { Component } from 'react';
-import {
-  Container, Row, Col, CardColumns, Card, CardImg, CardTitle,
-  CardBody, CardText, Button, Modal, ModalBody, ModalFooter,
-} from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 import { HashLink as Link } from 'react-router-hash-link';
+import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Notice from '../components/Notice';
 import './UserLanding.css';
 import './CertCheck.css';
 import config from '../config';
 import { invokeApig } from '../libs/awsLibs';
-
-class EnrolledCourses extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      courses:[], isLoading:false, modal:false,
-      certContents:{}
-    }
-  }
-  componentDidMount = async() => {
-    console.log('attempt to get enrolled courses');
-    try{
-      this.setState({isLoading:true});
-      var results = await this.loadEnrolledCourses();
-      this.setState({courses:results, isLoading: false});
-    }catch(e){
-      console.log('error getting enrolled courses');
-      console.log(e);
-    }
-  }
-  loadEnrolledCourses = () => {
-    return invokeApig({
-      endpoint: config.apiGateway.ENROLMENT_URL,
-      path: '/enrolment'
-    })
-
-  }
-  toggleModal = (e) =>{
-    this.setState({modal:!this.state.modal});
-
-    if(!this.state.modal){
-      //should update cert info
-      var courseIndex = e.target.dataset.course;
-      this.setState({
-        certContents: this.state.courses[courseIndex]
-      });
-    }
-  }
-  render = () => {
-    if(this.state.isLoading){
-      return <Notice content="Checking enrolled courses ..." />
-    };
-
-    var enrolledCourses = this.state.courses.length === 0 ? (
-      <div>You haven't enrolled in any courses yet.</div>
-    ) : (
-      <CardColumns>{ this.state.courses.map( (c,i) => {
-        var courseComplete = c.progress.length >= c.publishedModuleCount;
-        return (<Card key={i} className={`${courseComplete ? 'border border-success' : ''}`}>
-          <CardBody>
-            <CardTitle><Link to={`/courses/toc/${c.courseId}`}>{c.name}</Link></CardTitle>
-            <CardText><strong>Progress</strong></CardText>
-            <CardText>{c.progress.length} of {c.publishedModuleCount} modules attended</CardText>
-            {
-              courseComplete ?
-              <CardText className="text-success">
-                Course Completed <Button color="info" size="small" onClick={this.toggleModal} data-course={i}>View Cert</Button>
-              </CardText> : null
-            }
-          </CardBody>
-        </Card>)
-      })}</CardColumns>
-    );
-
-    var certDate = new Date(this.state.certContents.certIssued);
-    var courseName = this.state.certContents.name ? this.state.certContents.name.toUpperCase() : null;
-    return (
-      <Row>
-        <div className="col-12">
-          <h3>Enrolled Courses</h3>
-          <hr />
-          { enrolledCourses }
-          <Modal isOpen={this.state.modal} onOpened={this.handleModalOpen} size="lg" >
-            <ModalBody className="cert text-center">
-              <h2>CERTIFICATE OF COMPLETION</h2>
-              <p className="cert-text">This certifies that</p>
-              <p>{ this.props.currentUser.name }</p>
-              <p className="cert-text">has completed the short course entitled</p>
-              <p><strong>{ courseName }</strong></p>
-              <p><img className="cert-logo" src="/logos/learn.part1.png" alt="learn@AP" /></p>
-              <div className="w-100 d-flex justify-content-between  pb-4">
-                <span className="cert-text col-4 pt-4">
-                  <Link to={`/verify_cert?certNo=${this.state.certContents.certId}`}>
-                    <small>{this.state.certContents.certId}</small>
-                  </Link>
-                </span>
-                <span className="col-4"></span>
-                <span className="cert-text col-4 pt-4">{`${certDate.toLocaleString('en-us', {day:'numeric', month:'long', year:'numeric'})}`}</span>
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onClick={this.toggleModal}>OK</Button>
-            </ModalFooter>
-          </Modal>
-          <div className="col-12">
-            <p><Button color="primary" to="/courses" tag={Link}>Explore Courses</Button></p>
-          </div>
-        </div>
-      </Row>
-    )
-  }
-}
-
-class InvitedCourses extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      courses:[], isLoading:false
-    }
-  }
-  componentDidMount = async () => {
-    //load invited courses
-    this.setState({isLoading:true});
-    try{
-      var result = await this.loadInvitedCourses();
-      this.setState({courses:result, isLoading:false});
-    } catch(e){
-      console.log('error loading invited courses');
-      console.log(e);
-    };
-
-  }
-  loadInvitedCourses = () => {
-    return invokeApig({
-      endpoint: config.apiGateway.ENROLMENT_URL,
-      path: '/enrolment/invited',
-      queryParams: {email: this.props.email }
-    });
-  }
-  render(){
-    if(this.state.isLoading){
-      return <Notice content="Loading courses invites ..." />;
-    };
-
-    return (
-      <Row>
-        <Col xs="12">
-          <h3 id="invited">Invited Courses</h3>
-          <hr />
-        </Col>
-        <Col xs="12">{
-          this.state.courses.length === 0 ? (
-            <Row><Col><p>You haven't been invited to any courses yet ...</p></Col></Row>
-          ) : (
-            <Row><Col><CardColumns>{ this.state.courses.map( (course,i) => {
-              return (<Card key={i}>
-                <div style={ {overflow:'hidden', height:'3em'}}>
-                  <CardImg top src={ course.bg_pic } />
-                </div>
-                <CardBody>
-                  <CardTitle className="text-center" tag="h2">{ course.name }</CardTitle>
-                  <CardText className="lead">{course.description.split('\n')[0]}</CardText>
-                  { course.description.split('\n').length > 1 ? <CardText>...</CardText> : null }
-                  <Button className="mr-2 mb-2" color="info" tag={Link} to={`/courses/promo/${course.courseId}`} >View</Button>
-                </CardBody>
-              </Card>);
-            })}</CardColumns></Col></Row>
-          )
-        }</Col>
-      </Row>
-    )
-  }
-}
+import EnrolledCourses from '../components/UserLanding/EnrolledCourses';
+import InvitedCourses from '../components/UserLanding/InvitedCourses';
+import CourseManager from '../components/UserLanding/CourseManager';
 
 /*
 class CourseHighLights extends Component {
@@ -201,129 +39,44 @@ class CourseHighLights extends Component {
 }
 */
 
-class CourseManager extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      courses:[], isLoading:false
-    }
-  }
-  componentDidMount = async() => {
-    var handle=this;
-    console.log('attempt to get user related courses');
-    try {
-      handle.setState({isLoading:true});
-      var results = await this.getCourses();
-      handle.setState({courses:results, isLoading:false});
-    } catch(e){
-      console.log('error fetching user related courses');
-      console.log(e);
-    }
-  }
-  getCourses = () => {
-    return invokeApig({
-      path:'/courses/user'
-    })
-  }
-  deleteCourse = (id) => {
-    return invokeApig({
-      path:`/courses/${id}`,
-      method:'DELETE'
-    })
-  }
-  handleDelete = async(e) => {
-    var handle = this;
-
-    if( window.confirm('Really delete?') ){
-      console.log('now should relaly delete them;');
-      try {
-        await this.deleteCourse(e.target.id);
-        // TODO: remove from state instead of polling again from server
-        var results = await this.getCourses();
-        handle.setState({courses:results});
-      }catch(err){
-        console.log('Error trying to delete');
-        console.log(err);
-      }
-    }
-  }
-  render = () => {
-    if(this.state.isLoading){
-      return <Notice content="Loading courses ..." />;
-    };
-
-    return(
-      <Row>
-        <div className="col-12">
-          <h3 id="managed">Your Managed Courses</h3>
-          <hr />
-          <p>Applicable if you have admin or publisher roles</p>
-        </div>
-        <div className="col-12 col-md-8">
-          { this.state.courses.length === 0 ? (<Notice className="mb-2" content={this.state.isLoading ? 'Loading courses ...' : 'No courses found.'}/>) : (
-            this.state.courses.map( (e,i) => {
-              return (<Card key={i} className="mb-3">
-                <CardBody>
-                  <CardTitle>{e.name}</CardTitle>
-                  { e.description.split('\n').map( (p,key) => {
-                    return (<CardText key={key} className={key===0?'lead':''}>{p}</CardText>);
-                  })}
-                  <h4>Stats</h4>
-                  <p>Modules: (Published: {e.publishedModuleCount}, Total: {e.moduleCount}), Quiz questions: (y)</p>
-                  <p>Price: RM{e.price}</p>
-                  <Button className="mr-2 mb-2" color="info" tag={Link} to={`/courses/promo/${e.courseId}`}>Course Promo</Button>
-                  <Button className="mr-2 mb-2" color="info" tag={Link} to={`/courses/toc/${e.courseId}`}>Table of Contents</Button>
-                  <Button className="mr-2 mb-2" color="primary" tag={Link} to={`/user/builder/${e.courseId}`}>Manage</Button>
-                  <Button className="mr-2 mb-2" color="danger" id={e.courseId} onClick={this.handleDelete}>Delete</Button>
-                </CardBody>
-              </Card>);
-            })
-          )}
-        </div>
-        <div className="col-12">
-          <Link className="btn btn-primary" to="/courses/new">New Course</Link>
-        </div>
-      </Row>
-
-    )
-  }
-};
-
+/**
+ * The Constructor
+ * @param {json} props the props
+ * @returns {null} The sum of the two numbers.
+ */
 export default class UserLanding extends Component {
-  constructor(props){
-    super(props);
-    this.state = {courses:[], isLoading:false};
-  }
-  componentDidMount = async() => {
-    if(this.props.currentUser === null){
+  /**
+   * The Constructor
+   * @param {json} props the props
+   * @returns {null} The sum of the two numbers.
+   */
+  componentDidMount = async () => {
+    if (this.props.currentUser === null) {
       return;
     }
 
-    //attempt to check username
+    // attempt to check username
     try {
       await this.checkIdent();
-    } catch(e){
+    } catch (e) {
       console.log('error checking username');
       console.log(e);
     }
-
   }
-  checkIdent = () => {
-    return invokeApig({
-      endpoint: config.apiGateway.IDENT_URL,
-      method: 'POST',
-      path: '/check',
-      queryParams: { username: this.props.currentUser['cognito:username']}
-    });
-  }
-  render(){
-    var handle = this;
+  checkIdent = () => invokeApig({
+    endpoint: config.apiGateway.IDENT_URL,
+    method: 'POST',
+    path: '/check',
+    queryParams: { username: this.props.currentUser['cognito:username'] },
+  })
+  render = () => {
+    const handle = this;
 
-    if(this.props.isAuthenticating){
+    if (this.props.isAuthenticating) {
       return <Notice content="Authenticating session ..." />;
     }
 
-    if(!this.props.isAuthenticated){
+    if (!this.props.isAuthenticated) {
       return (<div>User is not authenticated</div>);
     }
 
@@ -341,18 +94,18 @@ export default class UserLanding extends Component {
           </div>
           <div className="col-12 col-md-8 mb-3">
             <ul>
-              <li><Link to="#enrolled">Enrolled Courses</Link></li>
-              <li><Link to="#invited">Invited Courses</Link></li>
+              <li><Link href="/" to="#enrolled">Enrolled Courses</Link></li>
+              <li><Link href="/" to="#invited">Invited Courses</Link></li>
               {
                 /* only allow course to be managed by an admin for now */
                 this.props.currentUser['cognito:groups'].includes('admin') ?
-                  <li><Link to="#managed">Managed Courses</Link></li> :
+                  <li><Link href="/" to="#managed">Managed Courses</Link></li> :
                   null
               }
             </ul>
           </div>
         </Row>
-        <EnrolledCourses { ...this.props }/>
+        <EnrolledCourses {...this.props} />
         <InvitedCourses email={this.props.currentUser.email} />
         {
           /*
@@ -365,6 +118,16 @@ export default class UserLanding extends Component {
           this.props.currentUser['cognito:groups'].includes('admin') ? <CourseManager /> : null
         }
       </Container>
-    )
+    );
   }
 }
+
+UserLanding.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  isAuthenticating: PropTypes.bool.isRequired,
+  currentUser: PropTypes.shape(),
+};
+
+UserLanding.defaultProps = {
+  currentUser: {},
+};
